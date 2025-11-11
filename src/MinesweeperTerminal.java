fba3534937ad0dd8cb9c9f3f7c4b5c5f9f880568
jschnars-hbrs <julian.schnars@smail.emt.h-brs.de>
2025-11-11 09:07:41 +0100

    Added classes
    Added Classes "MinesweeperTerminal" and "MinesweeperUI"

---
+++ src/MinesweeperTerminal.java
@@ -0,0 +1,17 @@
+public class MinesweeperTerminal extends MinesweeperUI {
+
+    public MinesweeperTerminal() {
+        super();
+    }
+
+    @Override
+    public void bekommeEingabe() {
+        System.out.println("Minesweeper Terminal Eingabe");
+    }
+
+    @Override
+    public void gebeAus() {
+        System.out.println("Minesweeper Terminal Ausgabe");
+    }
+    
+}
---
+++ src/MinesweeperUI.java
@@ -0,0 +1,24 @@
+public class MinesweeperUI {
+
+    enum Schwierigkeit{
+        LEICHT,
+        MITTEL,
+        SCHWER
+    }
+
+    protected MinesweeperSpielLogik spielLogik;
+    protected Schwierigkeit schwierigkeit;
+
+    public MinesweeperUI() {
+    }
+
+    public void bekommeEingabe() {
+    }
+
+    public void gebeAus() {
+    }
+
+    private void waehleSchwierigkeit(Schwierigkeit schwierigkeit) {
+    }
+    
+}
