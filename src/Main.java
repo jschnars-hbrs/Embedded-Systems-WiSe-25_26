42ec1b63be1f49109db3e1a6909a58f549b45b96
Tabea <tabea.barteldrees@smail.emt.h-brs.de>
2025-11-13 00:03:54 +0100

    Methode aufdecken() für die Minenfeldklasse begonnen. Markierte Zellen werden nicht aufgedeckt. Bei unmarkierten Zellen mit einer Mine gibt die Methode true zurück.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
  
   public static void main(String[] args) {
     try {
-         Minenfeld Testfeld = new Minenfeld(10, 10, 101);
+         Minenfeld Testfeld = new Minenfeld(20, 20, 45);
   
     } catch (ArithmeticException e) {
       System.out.println("Es wurde versucht ein Spielfeld mit mehr Minen zu generieren als auf dem Spielfeld Platz haben.");
--- src/Minenfeld.java
+++ src/Minenfeld.java
@@ -8,6 +8,11 @@ public class Minenfeld {
     int spaltenAnzahl;
     int minenAnzahl;
 
+    Zelle[][] testfeld;
+
+
+    
+
     //DEBUG Methode
     private void printSpielfeld(){
          for (int zeilenIndex = 0; zeilenIndex < this.zeilenAnzahl; zeilenIndex++) {
@@ -22,6 +27,48 @@ public class Minenfeld {
             System.out.println("");
         }
     }
+
+    private  void erstelleTestfeld(){
+        this.testfeld = new Zelle[6][6];
+
+        for (int zeilenIndex = 0; zeilenIndex < 6; zeilenIndex++) {
+            for (int spaltenIndex = 0; spaltenIndex < 6; spaltenIndex++) {
+                this.testfeld[zeilenIndex][spaltenIndex] = new Zelle();
+            }
+        }
+
+        //platziere Minen
+        this.testfeld[2][2].setzeMine();
+        this.testfeld[4][3].setzeMine();
+        this.testfeld[5][4].setzeMine();
+
+        this.testfeld[1][1].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[1][2].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[1][3].zaehleAngerenzendeMinenHochUmEins();
+
+        this.testfeld[2][1].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[2][3].zaehleAngerenzendeMinenHochUmEins();
+
+        this.testfeld[3][1].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[3][2].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[3][2].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[3][3].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[3][3].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[3][4].zaehleAngerenzendeMinenHochUmEins();
+
+        this.testfeld[4][2].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[4][4].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[4][4].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[4][5].zaehleAngerenzendeMinenHochUmEins();
+
+        this.testfeld[5][2].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[5][3].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[5][3].zaehleAngerenzendeMinenHochUmEins();
+        this.testfeld[5][5].zaehleAngerenzendeMinenHochUmEins();
+
+
+    }
+
     
     //private Methoden    
     private void platziereMinen(int zuVerteilendeMinen, Random Zufallsgenerator){
@@ -117,10 +164,37 @@ public class Minenfeld {
         platziereMinen(this.minenAnzahl, Zufallsgenerator);
         zaehleMinen();
         printSpielfeld(); //DEBUG Print
+
+        /* 
+        erstelleTestfeld(); //DEBUG Print
+        this.spaltenAnzahl=6;
+        this.zeilenAnzahl=6;
+        this.spielfeld = this.testfeld;
+        printSpielfeld(); //DEBUG Print
         
+        if(aufdecken(5, 4) == true){
+            System.out.println("Mine getroffen");
+        }
+        */
     }
 
-    //aufdecken(zeilenNummer: int, spaltenNummer: int): Zelle.istMine
+    
+    public boolean aufdecken(int zeilenNummer, int spaltenNummer){
+         
+        if(this.spielfeld[zeilenNummer][spaltenNummer].gebeIstMarkiertZustand() == true){//Ist die Zelle markiert & darf nicht aufgedeckt werden?
+            return false;
+        }else if(this.spielfeld[zeilenNummer][spaltenNummer].gebeIstMineZustand() == true){ //Liegt dort eine Mine?
+            this.spielfeld[zeilenNummer][spaltenNummer].deckeAuf();
+            return true;
+        }else if(this.spielfeld[zeilenNummer][spaltenNummer].gebeAnzahlAngrenzenderMinen() > 0){ //Zelle mit Zahl?
+            this.spielfeld[zeilenNummer][spaltenNummer].deckeAuf();
+            return false;
+        }else{
+            //TODO Add Breath-first search
+            return false;
+        }
+        
+    }
     //wechselMarkierung(zeilenNummer: int, spaltenNummer: int)
     //gebeFeld(zeilenNummer, spaltenNummer): Zelle
 }
