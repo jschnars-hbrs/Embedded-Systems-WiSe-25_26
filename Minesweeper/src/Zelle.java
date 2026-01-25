d609b4b1db97f7826cdf97282b20069f09c2bf75
Tabea <tabea.barteldrees@smail.emt.h-brs.de>
2026-01-25 21:03:55 +0100

    Kommentare zur Klasse Zelle hinzugefügt

--- Minesweeper/src/Zelle.java
+++ Minesweeper/src/Zelle.java
@@ -6,18 +6,19 @@ public class Zelle {
     private boolean istMarkiert = false;
     private int angrenzendeMinen = 0;
 
+    //Zugrifffunktionen von istMine
     public boolean gebeIstMineZustand(){
         return this.istMine;
     }
 
     public void setzeMine(){
-        if (this.istMine == true){
+        if (this.istMine == true){// Wenn bereits eine Mine platziert wurde, darf keine weitere platziert werden.
             throw new ArithmeticException("Auf dem Feld befindet sich bereits eine Mine.");
         }
         this.istMine = true;
     }
 
-
+    //Zugrifffunktionen von istAufgedeckt
     public boolean gebeIstAufgedecktZustand(){
         return this.istAufgedeckt;
     }
@@ -28,23 +29,24 @@ public class Zelle {
         }
     }
 
-
+    //Zugrifffunktionen von istMarkiert
     public boolean gebeIstMarkiertZustand(){
         return this.istMarkiert;
     }
 
     public void wechselMarkierung(){
-        if(!this.istAufgedeckt){
+        if(!this.istAufgedeckt){    //Nur verdeckte Zellen dürfen markiert werden
             this.istMarkiert = !this.istMarkiert;
         }
     }
 
+    //Zugrifffunktionen von angrenzendeMinen
     public int gebeAnzahlAngrenzenderMinen(){
         return this.angrenzendeMinen;
     }
 
     public void zaehleAngerenzendeMinenHochUmEins(){
-        if(this.angrenzendeMinen >= 8)
+        if(this.angrenzendeMinen >= 8)  //Es können nicht mehr als 8 Zellen mit Minen an einer Zelle angrenzen
         {
             throw new ArithmeticException("Die Anzahl angrenzender Minen kann nicht größer 8 sein.");
         }
