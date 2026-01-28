0356f3514914e27b604b8ddc96f54e6134918139
Pan <jan.pennnemann@brsmotorsport.de>
2026-01-28 21:15:23 +0100

    Added Watchface and ScreenManager

---
+++ Stoppuhr/Stoppuhr/Src/ScreenManager.cpp
@@ -0,0 +1,88 @@
+#include "ScreenManager.h"
+
+ScreenManager::ScreenManager(DigitalButton * button1, DigitalButton * button2, DigitalButton * button3, DigitalButton * user)
+{
+    m_numScreens = 0;
+    m_currentScreen = -1;
+    m_button1 = button1;
+    m_button2 = button2;
+    m_button3 = button3;
+    m_button_user = user;
+    // Possibly reserver space for watchfaces
+    m_watchfaces.reserve(5);
+}
+
+bool ScreenManager::setScreen(int numScreen)
+{
+    if (numScreen < 0 || numScreen >= m_numScreens)
+    {
+        return false;
+    }
+    m_currentScreen = numScreen;
+    return true;
+}
+bool ScreenManager::setNextScreen()
+{
+    if (m_numScreens == 0)
+    {
+        return false;
+    }
+    m_currentScreen = (m_currentScreen + 1) % m_numScreens;
+    return true;
+}
+bool ScreenManager::setPrevScreen()
+{
+    if (m_numScreens == 0)
+    {
+        return false;
+    }
+    m_currentScreen = (m_currentScreen - 1 + m_numScreens) % m_numScreens;
+    return true;
+}
+void ScreenManager::addWatchFace(Watchface *watchface)
+{
+    m_watchfaces.push_back(watchface);
+    m_numScreens++;
+}
+bool ScreenManager::removeWatchFace(int numScreen)
+{
+    if (numScreen < 0 || numScreen >= m_numScreens)
+    {
+        return false;
+    }
+    m_watchfaces.erase(numScreen+m_watchfaces.begin());
+    m_numScreens--;
+    return true;
+}
+
+bool ScreenManager::removeWatchFace(Watchface * watchface)
+{
+    auto it = std::find(m_watchfaces.begin(), m_watchfaces.end(), watchface);
+    if (it != m_watchfaces.end())
+    {
+        m_watchfaces.erase(it);
+        m_numScreens--;
+        return true;
+    }
+    return false;
+}
+void ScreenManager::update(){
+    if (m_currentScreen < 0 || m_currentScreen >= m_numScreens)
+    {
+        return;
+    }
+
+    m_watchfaces[m_currentScreen]->update();
+    Watchface::takeActionReturnValues action = m_watchfaces[m_currentScreen]->handleButtons(m_button1, m_button2, m_button3, m_button_user);
+    switch(action)
+    {
+        case Watchface::NEXT_SCREEN:
+            setNextScreen();
+            break;
+        case Watchface::PREVIOUS_SCRREEN:
+            setPrevScreen();
+            break;
+        default:
+            break;
+    }
+}
---
+++ Stoppuhr/Stoppuhr/Src/ScreenManager.h
@@ -0,0 +1,37 @@
+#ifndef SCREENMANAGER_H_INCLUDED
+#define SCREENMANAGER_H_INCLUDED
+
+#include "EmbSysLib.h"
+#include "config.h"
+#include <vector>
+#include <algorithm>
+class Watchface{
+public:
+    enum takeActionReturnValues {NO_ACTION, NEXT_SCREEN, PREVIOUS_SCRREEN};
+    virtual void update()=0;
+    virtual takeActionReturnValues handleButtons(DigitalButton * button1, DigitalButton * button2, DigitalButton * button3, DigitalButton * button_user)=0;
+};
+
+class ScreenManager: TaskManager::Task {
+public:
+    ScreenManager(DigitalButton * button1, DigitalButton * button2, DigitalButton * button3, DigitalButton * user);
+    bool setScreen(int numScreen);
+    bool setNextScreen();
+    bool setPrevScreen();
+    void addWatchFace(Watchface *watchface);
+    bool removeWatchFace(int numScreen);
+    bool removeWatchFace(Watchface * watchface);
+    void update();
+private:
+    ScreenGraphic * m_screenGraphic;
+    int m_numScreens;
+    int m_currentScreen;
+    std::vector<Watchface *> m_watchfaces;
+    DigitalButton * m_button1;
+    DigitalButton * m_button2;
+    DigitalButton * m_button3;
+    DigitalButton * m_button_user;
+};
+
+
+#endif
