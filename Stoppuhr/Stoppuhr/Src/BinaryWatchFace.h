6b3023f69af0d8b01f3e7a5f7f14984c55c59ecc
lcarranza7 <113393562+lcarranza7@users.noreply.github.com>
2026-01-29 15:59:30 +0100

    Update BinaryWatchFace.h

--- Stoppuhr/Stoppuhr/Src/BinaryWatchFace.h
+++ Stoppuhr/Stoppuhr/Src/BinaryWatchFace.h
@@ -1,30 +1,63 @@
-#pragma once
+/*!
+\file   BinaryWatchFace.h
+*/
+#ifndef binarywatchface_h_included
+#define binarywatchface_h_included
 
-#include "ScreenGraphic.h"
-#include "StopUhr.h"
-#include "WatchFace.h" // Passe diesen Include an den tatsächlichen Dateinamen an (z.B. AbstractWatchFace.h)
+#include "EmbSysLib.h"
+#include "ScreenManager.h"
+#include "StopUhrTimer.h"
 
-class BinaryWatchFace : public WatchFace
-{
-public:
-    // Konstruktor: Erwartet einen Zeiger auf die StopUhr-Logik (Dependency Injection)
-    BinaryWatchFace(StopUhr* stopwatch);
+using namespace EmbSysLib::Hw;
+using namespace EmbSysLib::Dev;
+using namespace EmbSysLib::Ctrl;
 
-    // Implementierung der abstrakten Methode aus WatchFace
-    void display(ScreenGraphic* dev) override;
+// Makro für Farbkonvertierung (wie in BouncingWatchFace)
+#define RGB2COLOR(red, green, blue ) \
+    ( (((blue )& 0xF8) >> 3)  /* 5 bit,  0.. 4 */\
+     |(((green)& 0xFC) << 3)  /* 6 bit,  5..10 */\
+     |(((red  )& 0xF8) << 8)) /* 5 bit, 11..15 */
 
-    // Button-Handler (muss implementiert werden, auch wenn leer)
-    // Passe die Signatur an, falls das Interface anders definiert ist
-    // T* handleButtons(Button* btn1, Button* btn2, Button* btn3) override;
+class BinaryWatchFace : public Watchface {
 
-private:
-    StopUhr* stopwatch;
-    bool firstRun {true};
+    public:
+        // Konstruktor passend zur Architektur
+        BinaryWatchFace(Timer_Mcu *timer, DisplayGraphic * dispGraphic, ScreenGraphic * lcd);
+        
+        // Überschriebene Methoden aus Watchface
+        void update() override;
+        void changed_to() override;
+        takeActionReturnValues handleButtons(DigitalButton * button1, DigitalButton * button2, DigitalButton * button3, DigitalButton * button_user) override;
 
-    // Private Hilfsmethoden zum Zeichnen
-    void drawFilledCircle(ScreenGraphic* dev, int cx, int cy, int radius, WORD color);
-    void drawCircleOutline(ScreenGraphic* dev, int cx, int cy, int radius, WORD color);
-    
-    // Zeichnet eine vertikale Bit-Spalte (für eine Dezimalziffer)
-    void drawBinaryDigit(ScreenGraphic* dev, int x, int yBase, int digit, int rows, int radius, int gap);
+    private:
+        // Zeitmanagement
+        StopUhrTimer myStoppWatch;
+        
+        // Grafik-Pointer
+        DisplayGraphic * dispGraphic;
+        ScreenGraphic * lcd;
+
+        // Farben
+        int colorBlack   = RGB2COLOR(0, 0, 0);
+        int colorWhite   = RGB2COLOR(255, 255, 255);
+        int colorMagenta = RGB2COLOR(255, 0, 255); // Für aktive Bits
+        int colorDark    = RGB2COLOR(50, 50, 50);  // Für inaktive Bits (Rahmen/Hintergrund)
+
+        // Geometrie-Parameter für die Kreise
+        int radius = 20;
+        int gap = 10;
+        int colGap = 15;
+        int groupGap = 50;
+        int startX = 0;
+        int yBase = 0;
+
+        // Hilfsmethoden zum Zeichnen
+        void drawFilledCircle(int cx, int cy, int radius, int color);
+        void drawCircleOutline(int cx, int cy, int radius, int color);
+        void drawBinaryDigit(int x, int yBase, int digit, int rows, int radius, int gap);
+        void drawStaticLayout(); // Zeichnet Text und Skala (nur einmal)
+        
+        // Status
+        bool isSetup = false;
 };
+#endif
