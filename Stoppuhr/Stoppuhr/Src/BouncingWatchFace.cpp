29e38f86eba635846940223715089e49a258043a
Pan <jan.pennnemann@brsmotorsport.de>
2026-01-29 12:27:18 +0100

    Fixed some git issues

--- Stoppuhr/Stoppuhr/Src/BouncingWatchFace.cpp
+++ Stoppuhr/Stoppuhr/Src/BouncingWatchFace.cpp
@@ -1,39 +1,214 @@
-#include "BasicWatchface.h"
+/*!
+\file   BouncingWatchFace.cpp
+*/
+#include "BouncingWatchFace.h"
 #include <stdio.h>
 
-BasicWatchface::BasicWatchface(Timer_Mcu  *timer,DisplayGraphic * in_dispGraphic,ScreenGraphic * in_lcd): stopwatch(timer)
+BouncingWatchFace::BouncingWatchFace(Timer_Mcu  *timer,DisplayGraphic * dispGraphic, ScreenGraphic * lcd): myStoppWatch(timer)
 {
-      dispGraphic=in_dispGraphic;
-      lcd=in_lcd;
-      //dispGraphic->setTextColor( Color::White    );
-      //dispGraphic->setBackColor( Color::DarkBlue );
-      }
-void BasicWatchface::update()
-{
-    char txt[1000]="Hello World";
-    dispGraphic->clear();
-    // Update display with stopwatch time
-    DWORD time = stopwatch.getPassedTime();
-    // Code to update the display goes here
-    printf(txt,"%02lu:%02lu.%03lu", (time / 60000), (time / 1000) % 60, time % 1000);
-    dispGraphic->gotoPixelPos(0,0);
-    dispGraphic->putString(txt);
-    lcd->refresh();
+    this->dispGraphic = dispGraphic;
+    this->lcd = lcd;
+    boundingBoxWidth = dispGraphic->getWidth();
+    boundingBoxHeight = dispGraphic->getHeight();
+    posX = boundingBoxWidth/2 - bounceObjectWidth/2;    //Centering the bounceObjekt in X
+    posY = boundingBoxHeight/2 - bounceObjectHeight/2;  //Centering the bounceObjekt in Y
 
 }
 
-BasicWatchface::takeActionReturnValues BasicWatchface::handleButtons(DigitalButton * button1, DigitalButton * button2, DigitalButton * button3, DigitalButton * button_user)
+void BouncingWatchFace::setUp()
+{
+    dispGraphic->setZoom(this->zoomFactor);
+    dispGraphic->setTextColor( this->colorWhite);
+    dispGraphic->setBackColor( this->colorBlack);
+    dispGraphic->clear();
+    lcd->refresh();
+}
+
+void BouncingWatchFace::update()
+{
+
+    if(this->setUpStatus == false)
+    {
+        this->setUpStatus = true;
+        this->setUp();
+    }
+
+
+    if (this->getIfRunning())
+    {
+        this->upDateCurrentTime();
+        this->upDateDisplayTime();
+        this->upDateAnimation();
+    }
+
+    this->upDateDisplay();
+
+    /*
+    LD1.set( Btn1.get() );
+    LD2.set( Btn2.get() );
+
+    this->checkStartStoppBTN();
+    this->checkResetBTN();*/
+
+}
+
+//Graphics
+void BouncingWatchFace::upDateDisplay()
+{
+    dispGraphic->gotoPixelPos(this->posX, this->posY);
+    dispGraphic->putString(this->displaytime);
+    //char txt[6] = "hello";
+    //dispGraphic->putString(txt);
+    lcd->refresh();
+}
+
+void BouncingWatchFace::upDateDisplayTime()
+{
+
+    this->minuts=int(this->currenttime/60)%100;
+    this->sec=int(this->currenttime)%60;
+    this->milliSec=int(this->currenttime*1000)%1000;
+    this->displaytime[0] = (this->minuts/10)+48;
+    this->displaytime[1] = this->minuts%10+48;
+    this->displaytime[3] = this->sec/10+48;
+    this->displaytime[4] = this->sec%10+48;
+    this->displaytime[6] = this->milliSec/100+48;
+    this->displaytime[7] = this->milliSec/10%10+48;
+    this->displaytime[8] = this->milliSec%10+48;
+
+}
+
+void BouncingWatchFace::changeColor()
+{
+    this-> currentColor +=1;
+    if (this->currentColor >= 4)
+    {
+        this->currentColor = 0;
+    }
+
+    switch (currentColor)
+    {
+    case 0:
+        dispGraphic->setTextColor( this->colorWhite );
+        break;
+    case 1:
+        dispGraphic->setTextColor( this->colorMagenta);
+        break;
+    case 2:
+        dispGraphic->setTextColor( this->colorCyan );
+        break;
+    case 3:
+        dispGraphic->setTextColor( this->colorYellow);
+        break;
+    }
+    dispGraphic->clear();
+}
+
+//Animation
+void BouncingWatchFace::upDateAnimation()
+{
+    if ((this->currenttime-this->lastFrameUpdate)>=1/this->framesPerSecond)
+    {
+        this->upDatePos();
+        this->checkForBounce();
+        this->delFrameTraces();
+    }
+}
+
+void BouncingWatchFace::upDatePos()
+{
+
+    this->lastFrameUpdate = this->currenttime;
+    this->posXOld = this->posX;
+    this->posYOld = this->posY;
+    this->posX+=this->speedX;
+    this->posY+=this->speedY;
+}
+
+void BouncingWatchFace::checkForBounce()
+{
+    if ((this->posX+this->bounceObjectWidth+this->zoomFactor>= this->boundingBoxWidth)||(this->posX-this->zoomFactor<=0))
+    {
+        this->speedX *= -1; //reverse direction
+        this->changeColor();
+    }
+    if ((this->posY+this->bounceObjectHeight+this->zoomFactor>=this->boundingBoxHeight)||(this->posY-this->zoomFactor<=0))
+    {
+        this->speedY *= -1; //reverse direction
+        this->changeColor();
+    }
+}
+
+void BouncingWatchFace::delFrameTraces()
+{
+    if(this->speedX > 0)
+    {
+        dispGraphic->gotoPixelPos(this->posXOld-this->zoomFactor, this->posY);
+    }
+    else
+    {
+        dispGraphic->gotoPixelPos(this->posX + this->bounceObjectWidth-1, this->posY);
+    }
+    dispGraphic->putString(" ");
+}
+
+//time management
+float BouncingWatchFace::getTime()
+{
+    return this->myStoppWatch.getPassedTime();
+}
+
+void BouncingWatchFace::upDateCurrentTime()
+{
+    this->currenttime=this->getTime();
+}
+
+//controls
+bool BouncingWatchFace::getIfRunning()
+{
+    if(this->run == true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void BouncingWatchFace::resetCurrentTime()
+{
+    if(this->allowReset==true)
+    {
+        myStoppWatch.reset();
+        this->currenttime = 0;
+        this->lastFrameUpdate = 0;
+        this->displaytime[0] = '0';
+        this->displaytime[1] = '0';
+        this->displaytime[3] = '0';
+        this->displaytime[4] = '0';
+        this->displaytime[6] = '0';
+        this->displaytime[7] = '0';
+        this->displaytime[8] = '0';
+    }
+}
+
+BouncingWatchFace::takeActionReturnValues BouncingWatchFace::handleButtons(DigitalButton * button1, DigitalButton * button2, DigitalButton * button3, DigitalButton * button_user)
 {
     if(button1->getAction() == DigitalButton::ACTIVATED) {
-        stopwatch.start();
+        myStoppWatch.start();
+        this->run = true;
+        this->allowReset = false;
         return NO_ACTION;
     }
     if(button2->getAction() == DigitalButton::ACTIVATED) {
-       stopwatch.stop();
+       myStoppWatch.stop();
+       this->run = false;
+       this->allowReset = true;
         return NO_ACTION;
     }
     if(button3->getAction() == DigitalButton::ACTIVATED) {
-       stopwatch.reset();
+       resetCurrentTime();
         return NO_ACTION;
     }
     if(button_user->getAction() == DigitalButton::ACTIVATED) {
