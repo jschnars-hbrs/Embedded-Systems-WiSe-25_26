e20e4f7c3540ee0398f97ce98980d6cffb8c3238
Pan <jan.pennnemann@brsmotorsport.de>
2026-01-29 12:19:19 +0100

    Revert "Funktionierendes BouncingWatchFace h und cpp hinzugefügt."
    This reverts commit d924f40ceb4c5df87014513c544db8e9a88a62ed.

--- Stoppuhr/Stoppuhr/Src/BouncingWatchFace.h
+++ Stoppuhr/Stoppuhr/Src/BouncingWatchFace.h
@@ -1,48 +1,27 @@
 /*!
 \file   BouncingWatchFace.h
 */
-#ifndef bouncingwatchface_h_included
-#define bouncingwatchface_h_included
 
-#include "EmbSysLib.h"
-#include "ScreenManager.h"
-#include "StopUhrTimer.h"
-//#include "Color.h"
-using namespace EmbSysLib::Hw;
-using namespace EmbSysLib::Dev;
-using namespace EmbSysLib::Ctrl;
-
-#define RGB2COLOR(red, green, blue ) \
-    ( (((blue )& 0xF8) >> 3)  /* 5 bit,  0.. 4 */\
-     |(((green)& 0xFC) << 3)  /* 6 bit,  5..10 */\
-     |(((red  )& 0xF8) << 8)) /* 5 bit, 11..15 */
+#include "config.h"
 
 #include "StopUhrTimer.h"
-class BouncingWatchFace : public Watchface {
+class BouncingWatchFace : public WatchFace 
+{
 
     public:
-        BouncingWatchFace(Timer_Mcu  *timer,DisplayGraphic * dispGraphic, ScreenGraphic * lcd);
-        void update() override;
-        takeActionReturnValues handleButtons(DigitalButton * button1, DigitalButton * button2, DigitalButton * button3, DigitalButton * button_user) override;
+        BouncingWatchFace(Timer *timer): myStoppWatch(timer)
+    {
+    }
+    void update();
 
     private:
         //Time management
         StopUhrTimer myStoppWatch;
         float currenttime = 0;
-        int milliSec = 0;
-        int sec = 0;
-        int minuts = 0;
 
         //Graphics
-        int colorBlack = RGB2COLOR(   0,    0,   0 );
-        int colorWhite = RGB2COLOR(   255,    255,   255 );
-        int colorYellow = RGB2COLOR( 255,  255,   0 );
-        int colorCyan = RGB2COLOR(   0,  255, 255 );
-        int colorMagenta = RGB2COLOR( 255,    0, 255 );
-        DisplayGraphic * dispGraphic;
-        ScreenGraphic * lcd;
-        WORD boundingBoxWidth =432;
-        WORD boundingBoxHeight=54;
+        WORD boundingBoxWidth = dispGraphic.getWidth();
+        WORD boundingBoxHeight = dispGraphic.getHeight();
 
         int zoomFactor = 6;
 
@@ -56,8 +35,8 @@ class BouncingWatchFace : public Watchface {
         int bounceObjectWidth = symbolWidth*zoomFactor*symbolCount;
         int bounceObjectHeight = symbolHeight*zoomFactor*lineCount;
 
-        int posX;    //Centering the bounceObjekt in X
-        int posY;  //Centering the bounceObjekt in Y
+        int posX = boundingBoxWidth/2 - bounceObjectWidth/2;    //Centering the bounceObjekt in X
+        int posY = boundingBoxHeight/2 - bounceObjectHeight/2;  //Centering the bounceObjekt in Y
         int posXOld;
         int posYOld;
         float speedX = 4;
@@ -69,33 +48,10 @@ class BouncingWatchFace : public Watchface {
 
         //controls
         bool setUpStatus = false;
-        bool run = false;
         bool allowReset = true;
-
-
-
 
         //Functions
         void setUp();
         void upDateDisplay();
 
-        //Graphics
-        void upDateDisplayTime();
-        void changeColor();
-
-        //Animations
-        void upDateAnimation();
-        void upDatePos();
-        void checkForBounce();
-        void delFrameTraces();
-
-        //Time management
-        float getTime();
-        void upDateCurrentTime();
-
-        //controls
-        bool getIfRunning();
-        void resetCurrentTime();
-
 };
-#endif
