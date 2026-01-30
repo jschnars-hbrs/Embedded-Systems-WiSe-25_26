a7f3a4382c621517429e9806d7de5021b1132c29
lcarranza7 <113393562+lcarranza7@users.noreply.github.com>
2026-01-30 16:35:34 +0100

    Update BinaryWatchFace.h

--- Stoppuhr/Stoppuhr/Src/BinaryWatchFace.h
+++ Stoppuhr/Stoppuhr/Src/BinaryWatchFace.h
@@ -12,7 +12,7 @@ using namespace EmbSysLib::Hw;
 using namespace EmbSysLib::Dev;
 using namespace EmbSysLib::Ctrl;
 
-// Makro für Farbkonvertierung (wie in BouncingWatchFace)
+// Makro für Farbkonvertierung
 #define RGB2COLOR(red, green, blue ) \
     ( (((blue )& 0xF8) >> 3)  /* 5 bit,  0.. 4 */\
      |(((green)& 0xFC) << 3)  /* 6 bit,  5..10 */\
@@ -21,43 +21,40 @@ using namespace EmbSysLib::Ctrl;
 class BinaryWatchFace : public Watchface {
 
     public:
-        // Konstruktor passend zur Architektur
         BinaryWatchFace(Timer_Mcu *timer, DisplayGraphic * dispGraphic, ScreenGraphic * lcd);
         
-        // Überschriebene Methoden aus Watchface
         void update() override;
         void changed_to() override;
         takeActionReturnValues handleButtons(DigitalButton * button1, DigitalButton * button2, DigitalButton * button3, DigitalButton * button_user) override;
 
     private:
-        // Zeitmanagement
         StopUhrTimer myStoppWatch;
-        
-        // Grafik-Pointer
         DisplayGraphic * dispGraphic;
         ScreenGraphic * lcd;
 
         // Farben
         int colorBlack   = RGB2COLOR(0, 0, 0);
         int colorWhite   = RGB2COLOR(255, 255, 255);
-        int colorMagenta = RGB2COLOR(255, 0, 255); // Für aktive Bits
-        int colorDark    = RGB2COLOR(50, 50, 50);  // Für inaktive Bits (Rahmen/Hintergrund)
+        int colorMagenta = RGB2COLOR(255, 0, 255); 
+        int colorDark    = RGB2COLOR(50, 50, 50); 
 
-        // Geometrie-Parameter für die Kreise
-        int radius = 20;
-        int gap = 10;
-        int colGap = 15;
-        int groupGap = 50;
+        // Geometrie-Parameter (VERKLEINERT)
+        int radius = 10;      // Kleinerer Radius (war 20)
+        int gap = 6;          // Abstand vertikal
+        int colGap = 8;       // Abstand zwischen Spalten
+        int groupGap = 35;    // Abstand zwischen Gruppen
         int startX = 0;
         int yBase = 0;
 
-        // Hilfsmethoden zum Zeichnen
+        // Hilfsmethoden
         void drawFilledCircle(int cx, int cy, int radius, int color);
         void drawCircleOutline(int cx, int cy, int radius, int color);
         void drawBinaryDigit(int x, int yBase, int digit, int rows, int radius, int gap);
-        void drawStaticLayout(); // Zeichnet Text und Skala (nur einmal)
+        void drawStaticLayout(); 
         
-        // Status
+        // Helper um Dezimalzahlen zu zeichnen
+        void drawDigitNumber(int x, int y, int number);
+
         bool isSetup = false;
 };
 #endif
