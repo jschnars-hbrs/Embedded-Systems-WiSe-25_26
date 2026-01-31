fbc5cd84cde0ef3038e6473ae1c0758e6a5d27aa
jschnars-hbrs <julian.schnars@smail.emt.h-brs.de>
2026-01-31 09:52:33 +0100

    comment in main

--- Minesweeper/src/Main.java
+++ Minesweeper/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         MinesweeperSpielLogik logik = new MinesweeperSpielLogik();
-        MinesweeperUI ui = new MinesweeperGUI(logik);
-        //MinesweeperUI ui = new MinesweeperTerminal(logik);
+        MinesweeperUI ui = new MinesweeperGUI(logik);               // Grafisches User-Interface
+        //MinesweeperUI ui = new MinesweeperTerminal(logik);        // User Interface im Terminal
         ui.starten();
     }
 }
